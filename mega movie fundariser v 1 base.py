f6166fce4ff288af165cce3b96c58bffa785c7b8
SH20051230 <98988694+SH20051230@users.noreply.github.com>
2022-03-17 11:49:41 +1300

    Initial commit

---
+++ mega movie fundariser v 1 base.py
@@ -0,0 +1,25 @@
+#import statement
+
+#Functions
+
+#MAin routine
+
+#Did the user used the proggame before, if no give them the instructions
+
+#Loop to get tickets detail
+
+   #Get name(can't be blank)
+
+   #Get age(between 12-110)
+
+   #Get ticket price
+
+   #Ask for snacks
+
+   #Calculate snack price
+
+   #Payment method(sur charge for credit card)
+
+#calculate total files and profit
+
+#PRint details
